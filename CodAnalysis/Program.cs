c41f4e0b779d6e8c451c6ffb23ef6faf49f6ed89
Kurt Strecker <ksphotopro@gmail.com>
2018-07-29 22:54:05 -0400

    Add project files.

---
+++ CodAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
